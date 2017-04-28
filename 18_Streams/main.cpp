561d489c8f3b761f4e53af6108961ded9a04f42c
Mateusz <inz.mateusz.przybyla@gmail.com>
2017-04-28 18:17:19 +0200

    Added part 18.2

--- 18_Streams/main.cpp
+++ 18_Streams/main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 #include <cstdlib>
+#include <iomanip>
+#include <string>
 
 using namespace std;
 
 int main() {
+  //
+  // Part 18.1
+  //
+  cout << "Part: 18.1" << endl;
+
   cout << "Enter your age: " << endl;
 
   int age;
-  cin >> age;
+  //cin >> age;
+  age = 2;
 
   if (age <= 0) {
     cerr << "Oops!" << endl;
@@ -15,6 +23,39 @@ int main() {
   }
 
   cout << "You entered " << age << " years" << endl;
+
+  //
+  // Part 18.2
+  //
+  cout << "Part: 18.2" << endl;
+
+//  char buf[10]{'a'};
+//  cin >> setw(10) >> buf;
+//  cout << buf << endl;
+
+//  char ch;
+//  while (cin.get(ch))
+//    cout << ch;
+
+//  char strBuf[11];
+//  cin.get(strBuf, 11);
+//  cout << strBuf << endl;
+
+//  cin.get(strBuf, 11);
+//  cout << strBuf << endl;
+
+//  char strBuf[11];
+//  cin.getline(strBuf, 11);
+//  cout << strBuf << endl;
+
+//  cin.getline(strBuf, 11);
+//  cout << strBuf << endl;
+
+//  cout << cin.gcount() << " characters read" << endl;
+
+  string str;
+  getline(cin, str);
+  cout << str << endl;
 
   return 0;
 }
