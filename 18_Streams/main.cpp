3825080f71cbbf9b8d598bc9b3fddc06878262f4
Mateusz Przybyla <inz.mateusz.przybyla@gmail.com>
2017-05-04 23:12:25 +0200

    Started part 18.4

--- 18_Streams/main.cpp
+++ 18_Streams/main.cpp
@@ -57,5 +57,93 @@ int main() {
 //  getline(cin, str);
 //  cout << str << endl;
 
+  //
+  // Part 18.3
+  //
+  cout << "Part: 18.3" << endl;
+
+//  cout.setf(ios::showpos);
+//  cout << 27 << endl;
+//  cout.unsetf(ios::showpos);
+//  cout << 28 << endl;
+
+//  //cout.unsetf(ios::dec);
+//  //cout.setf(ios::hex);
+//  cout.setf(ios::hex, ios::basefield);
+//  cout << 27 << endl;
+
+//  cout << hex << 27 << endl;
+//  cout << 28 << endl;
+//  cout << dec << 29 << endl;
+
+//  cout << true << " " << false << endl;
+//  cout.setf(ios::boolalpha);
+//  cout << true << " " << false << endl;
+//  cout << noboolalpha << true << " " << false << endl;
+//  cout << boolalpha << true << " " << false << endl;
+
+//  cout << 5 << endl;
+//  cout.setf(ios::showpos);
+//  cout << 5 << endl;
+//  cout << noshowpos << 5 << endl;
+//  cout << showpos << 5 << endl;
+
+//  cout << 12345678.9 << endl;
+//  cout.setf(ios::uppercase);
+//  cout << 12345678.9 << endl;
+//  cout << nouppercase << 12345678.9 << endl;
+//  cout << uppercase << 12345678.9 << endl;
+
+//  cout << 27 << endl;
+//  cout.setf(ios::dec, ios::basefield);
+//  cout << 27 << endl;
+//  cout.setf(ios::oct, ios::basefield);
+//  cout << 27 << endl;
+//  cout.setf(ios::hex, ios::basefield);
+//  cout << 27 << endl;
+
+//  cout << dec << 27 << endl;
+//  cout << oct << 27 << endl;
+//  cout << hex << 27 << endl;
+
+//  cout << showpoint << endl;
+//  cout << setprecision(3) << 123.456 << endl;
+//  cout << setprecision(4) << 123.456 << endl;
+//  cout << setprecision(5) << 123.456 << endl;
+//  cout << setprecision(6) << 123.456 << endl;
+//  cout << setprecision(7) << 123.456 << endl;
+
+//  cout << fixed << endl;
+//  cout << setprecision(3) << 123.456 << endl;
+//  cout << setprecision(4) << 123.456 << endl;
+//  cout << setprecision(5) << 123.456 << endl;
+//  cout << setprecision(6) << 123.456 << endl;
+//  cout << setprecision(7) << 123.456 << endl;
+
+//  cout << scientific << endl;
+//  cout << setprecision(3) << 123.456 << endl;
+//  cout << setprecision(4) << 123.456 << endl;
+//  cout << setprecision(5) << 123.456 << endl;
+//  cout << setprecision(6) << 123.456 << endl;
+//  cout << setprecision(7) << 123.456 << endl;
+
+//  cout << -12345 << endl;
+//  cout << setw(10) << -12345 << endl;
+//  cout << setw(10) << left << -12345 << endl;
+//  cout << setw(10) << right << -12345 << endl;
+//  cout << setw(10) << internal << -12345 << endl;
+
+//  cout.fill('*');
+//  cout << -12345 << endl;
+//  cout << setw(10) << -12345 << endl;
+//  cout << setw(10) << left << -12345 << endl;
+//  cout << setw(10) << right << -12345 << endl;
+//  cout << setw(10) << internal << -12345 << endl;
+
+  //
+  // Part 18.4
+  //
+  cout << "Part: 18.4" << endl;
+
   return 0;
 }
